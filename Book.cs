4a2cc5d4d902363463a376e2fcaefb25c665e946
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2020-05-01 04:02:58 -0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROAssignment2.Models
+{
+    public class Book
+    {
+
+        public int bookID { get; set; }
+
+        public String summary { get; set; }
+
+        public String title { get; set; }
+
+        public String libraryAddress { get; set; }
+
+        public int libraryID { get; set; }
+    }
+}
