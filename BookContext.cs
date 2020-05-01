7af29b98b1acdf98e73a09585f26349eeadb0801
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2020-05-01 04:03:07 -0400

    Add files via upload

---
+++ BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PROAssignment2.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
